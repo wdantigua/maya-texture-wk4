5e77a8d0d70f206302e334416d8377ab7cf0c17b
Wilson Diaz <diazw291@newschool.edu>
2016-09-27 13:35:22 -0400

    created city block assets and textures

---
+++ building_cityBlock/Assets/moveShip.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+public class moveShip : MonoBehaviour
+{
+	public float moveSpeed = 10f;
+	public float turnSpeed = 50f;
+	public float autoSpeed = 30f;
+
+	void Update ()
+	{
+
+		if (Input.GetKey (KeyCode.UpArrow))
+		{
+			transform.Translate (-Vector3.forward * moveSpeed * Time.deltaTime);
+
+		}
+
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			GetComponent <AudioSource> ().Play();
+		}
+		else 
+		{
+
+			transform.position -= transform.forward * Time.deltaTime * autoSpeed;
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+		if(Input.GetKey(KeyCode.DownArrow))
+			transform.position += transform.forward * Time.deltaTime * (autoSpeed - 15);
+
+		if(Input.GetKey(KeyCode.A))
+			transform.Rotate(transform.forward, turnSpeed * Time.deltaTime);
+
+	}
+}
---
+++ building_cityBlock/Assets/planeControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeControl : MonoBehaviour {
+
+
+	public float speed = 10;
+	public float turnSpeed = 15;
+	public float yawAmount = 25;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//gets a 1 unit line(vector3) representation of a line that goes in one direction relative the other transformsto  relative to the 3d space
+
+		float turnAround = Input.GetAxis ("Horizontal")* turnSpeed;
+
+		float yaw = Input.GetAxis ("Horizontal") * -yawAmount;
+
+		Vector3 rotationAmt = new Vector3 (transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + turnAround, yaw);
+
+		transform.rotation = Quaternion.Euler (rotationAmt);
+
+
+		rb.velocity = transform.forward*speed;
+
+
+	}
+}
